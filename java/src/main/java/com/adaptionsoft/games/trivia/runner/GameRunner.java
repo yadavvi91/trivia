862fdfe3dce80410e303f75091fd0cc46f7c2994
Vishal Yadav <yadavvi91@gmail.com>
2023-10-02 13:47:28 +0530

    main() uses `Random.nextInt()` to get new rolls

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,10 +1,5 @@
 
 package com.adaptionsoft.games.trivia.runner;
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.ConsoleUI;
@@ -14,40 +9,31 @@ import com.adaptionsoft.games.uglytrivia.UI;
 
 public class GameRunner {
 
-	public static void main(String[] args) throws IOException {
+	private static boolean notAWinner;
+
+	public static void main(String[] args) {
 		UI ui = new ConsoleUI(System.out);
 		Game aGame = new Game(ui);
 
-		// String line2 = readLine();
+		aGame.add("Chet");
+		aGame.add("Pat");
+		aGame.add("Sue");
 
-		File file = new File("src/main/resources/input.txt");
-		// BufferedInputStream stream = new BufferedInputStream(new FileInputStream(file));
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-		String line = reader.readLine();
-		String[] playerNames = line.split(" ");
-		for (String playerName : playerNames) {
-			aGame.add(playerName);
-		}
+		Random rand = new Random();
 
-		while ((line = reader.readLine()) != null) {
-			int roll = Integer.parseInt(line.trim());
-			aGame.roll(roll);
-			String answer = reader.readLine().trim();
-			if (answer.equalsIgnoreCase("correct")) {
-				boolean notAWinner = aGame.wasCorrectlyAnswered();
-				if (!notAWinner) break;
-			} else if (answer.equalsIgnoreCase("incorrect")) {
-				boolean notAWinner = aGame.wrongAnswer();
-				if (!notAWinner) break;
+		do {
+
+			aGame.roll(rand.nextInt(5) + 1);
+
+			if (rand.nextInt(9) == 7) {
+				notAWinner = aGame.wrongAnswer();
+			} else {
+				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-		}
-	}
 
-	private static String readLine() throws IOException {
-		String currentPlayerName = Files.readString(Path.of("src/main/resources/input.txt"), StandardCharsets.UTF_8);
-		String[] lines = currentPlayerName.split("\n");
-		String[] players = lines[0].split(" ");
-		System.out.println(Arrays.asList(players));
-		return null;
+
+
+		} while (notAWinner);
+
 	}
 }
