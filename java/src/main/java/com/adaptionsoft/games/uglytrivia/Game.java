674470b8d5a69c72b5a3f3f6acf490ab50f6473c
Vishal Yadav <yadavvi91@gmail.com>
2023-10-02 10:47:15 +0530

    Inlined a method

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -21,16 +21,12 @@ public class Game {
     public  Game(){
     	for (int i = 0; i < 50; i++) {
 			popQuestions.addLast("Pop Question " + i);
-			scienceQuestions.addLast(("Science Question " + i));
-			sportsQuestions.addLast(("Sports Question " + i));
-			rockQuestions.addLast(createRockQuestion(i));
+			scienceQuestions.addLast("Science Question " + i);
+			sportsQuestions.addLast("Sports Question " + i);
+			rockQuestions.addLast("Rock Question " + i);
     	}
     }
 
-	public String createRockQuestion(int index){
-		return "Rock Question " + index;
-	}
-	
 	public boolean isPlayable() {
 		return (howManyPlayers() >= 2);
 	}
